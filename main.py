5620feb48ea578d3a530db280f47ba80cb14827d
IvanTheFox <ivan.shepelev.06@inbox.ru>
2024-10-20 16:43:08 +0300

    Добавил условия конца игры

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 board = [
     ["_", "_", "_"],
     ["_", "_", "_"],
@@ -51,8 +53,6 @@ is_cross = False
 
 def show_board(): print("\n".join([" ".join([e for e in row]) for row in board]))
 
-  def show_board(): print("\n".join([" ".join([e for e in row]) for row in board]))
-
 def take_coordinates(s : str):
     s = s.split()
     if len(s) != 2: return None, None
@@ -63,3 +63,25 @@ def take_coordinates(s : str):
     if x > 3 or x < 1 or y > 3 or y < 1 or board[x - 1][y - 1] != "_":
         return None, None
     return x, y
+
+def check_game_end_condition():
+    global board, is_cross
+    current_symbol = "X" if is_cross else "O"
+    game_state = []
+    for row in range(3):
+        game_state.append([])
+        for col in range(3):
+            #print(board[row][col], " == ", current_symbol, " = ", board[row][col] == current_symbol)
+            game_state[-1].append(int(board[row][col] == current_symbol))
+    flag = False
+    for wp in win_patterns:
+        multiplied = deepcopy(game_state)
+        for row in range(3):
+            for col in range(3):
+                multiplied[row][col] *= wp[row][col]
+        #print(game_state, " * ", wp, " == ", multiplied)
+        if wp == multiplied:
+            flag = True
+            break
+
+    return flag
